29eca228eed2c4a8227320bc936144f3cb7fafac
zayanahmed925 <rjraju925@gmail.com>
2022-05-23 03:49:30 +0600

    get singel data

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors');
 const app = express()
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 //middleware
@@ -27,6 +27,13 @@ async function run() {
             const query = {};
             const cursor = toolsCollection.find(query);
             const tools = await cursor.toArray();
+            res.send(tools);
+        })
+        //single data load
+        app.get('/tools/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) };
+            const tools = await toolsCollection.findOne(query);
             res.send(tools);
         })
     }
