9cbd5b96035dc7dc73d5f045f44a57f094e121b5
zayanahmed925 <rjraju925@gmail.com>
2022-05-24 04:42:56 +0600

    admin role set

--- index.js
+++ index.js
@@ -84,10 +84,22 @@ async function run() {
             res.send({ result, token });
         })
         //get all user
-        app.get('/users', async (req, res) => {
+        app.get('/users', verifyJwt, async (req, res) => {
             const users = await userCollection.find().toArray();
             res.send(users)
         })
+        //make admin role
+        app.put('/user/admin/:email', verifyJwt, async (req, res) => {
+            const email = req.params.email;
+
+            const filter = { email: email };
+            const updateDoc = {
+                $set: { role: 'admin' },
+            };
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
     }
     finally {
 
