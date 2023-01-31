91d1c4fa98f2948a403d4c6a95e851117e695762
Zehadul <zehadul925@gmail.com>
2023-02-01 00:54:36 +0600

    checked

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
-//middleware
+//middleware 
 app.use(cors());
 app.use(express.json());
 
